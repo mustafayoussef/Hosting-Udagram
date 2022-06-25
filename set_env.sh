5d551ea4bbf311d887f3043abc6ce5deb569ca3b
Mustafa Youssef <mustafa.youssef@creiden.com>
2022-06-26 01:24:30 +0200

    first commit

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-2.c3wwykvd71fp.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export POSTGRES_PORT=5432
+export AWS_BUCKET=s3://mustafa-udagram
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
